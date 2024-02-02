65e9799c666ff06bf1286d74cd1cfb7c8213ab34
geaviozzi <geaviozzi@icloud.com>
2024-02-02 20:45:14 +0100

    changed also ml_csp file

--- ml_csp.py
+++ ml_csp.py
@@ -247,8 +247,11 @@ def modelling():
 
 
 
-def calculate_volatility(ds_name):
-        df = pd.read_csv(ds_name)
+def calculate_volatility():
+    datasets = ['AAPL.csv', 'AMZN.csv', 'SBUX.csv']
+    volatilities = {}
+    for dataset in datasets:
+        df = data_preparation(dataset)
         # calculating relative price: proportional change in stock price between a day and the dau before
         df['Price relative'] = ""
         for i in range(1, len(df.Date)):
@@ -261,35 +264,49 @@ def calculate_volatility(ds_name):
 
         # daily volatility
         DailyVolatility = statistics.stdev(df['Daily Return'][1:])
-        print("The daily volatility  is: {:.2%}".format(DailyVolatility))
+        #print("The daily volatility  is: {:.2%}".format(DailyVolatility))
 
         # annulized daily voltility
         AnnualizedDailyVolatilityCalendarDays = DailyVolatility * np.sqrt(365)
-        print("The annualized daily volatility measured in calendar days is: {:.2%}".format(
-            AnnualizedDailyVolatilityCalendarDays))
+        #print("The annualized daily volatility measured in calendar days is: {:.2%}".format(
+            #AnnualizedDailyVolatilityCalendarDays))
+        AnnualizedDailyVolatilityCalendarDays = round(AnnualizedDailyVolatilityCalendarDays, 2)
+        volatilities.update({dataset : AnnualizedDailyVolatilityCalendarDays})
+    #print(volatilities)
+    return volatilities
 
-        df = df.drop('Daily Return', axis=1)
-        df = df.drop('Price relative', axis=1)
-        return AnnualizedDailyVolatilityCalendarDays
+asset_volatilities = calculate_volatility()
 
-def build_portfolio_csp( min_investment, max_investment, risk_factor, min_expected_return):
-    domain = np.arange(0, max_investment + 1, 0.01)
+def build_portfolio_csp(min_investment, max_investment, risk_factor, min_expected_return):
+    domain = np.arange(0, max_investment+10, 10)
     max_for_each = max_investment / 3
     aapl = Variable('AAPL.csv', domain)
     amzn = Variable('AMZN.csv', domain)
     stbuks = Variable('SBUX.csv', domain)
     # [...]
-    
-    variables = [aapl, amzn, stbuks] #...
+
+    variables = [aapl, amzn, stbuks]  # ...
 
     def calculate_portfolio_volatility(*values):
-        var_values = dict(zip(variables, values))
+        #asset_volatilities = calculate_volatility()
 
-        file_names = [var_values[var] for var in variables]
+        #asset_volatilities =  {'AAPL.csv': 0.4, 'AMZN.csv': 0.4, 'SBUX.csv': 0.4}
 
-        total_volatility = sum(calculate_volatility(file_name) for file_name in file_names)
 
-        return total_volatility
+        # Calcolare la somma delle volatilità ponderate
+        weighted_volatilities_sum = sum(
+            value * asset_volatilities[asset] for value, asset in zip(values, asset_volatilities))
+
+        # Calcolare la somma totale degli investimenti
+        total_investment = sum(values)
+        if total_investment == 0:
+            return 0
+
+
+        portfolio_volatility = weighted_volatilities_sum / total_investment
+
+
+        return portfolio_volatility
     """
     def calculate_min_return(*values):
         y_train = []
@@ -303,7 +320,7 @@ def build_portfolio_csp( min_investment, max_investment, risk_factor, min_expect
             df['Date'] = pd.to_datetime(df['Date'])
             df = df.set_index('Date')
             df = df.dropna()
-            
+
             #splitting the dataset
             X = df.drop('Close', axis=1)
             Y = df['Close']
@@ -312,7 +329,7 @@ def build_portfolio_csp( min_investment, max_investment, risk_factor, min_expect
 
             last_value_test = y_test[-1]
             last_value_train = y_train[-1]
-            
+
             err.append((last_value_test - last_value_train)*(values[filenames.index(file)]/last_value_test))
         err_value=sum(err)
         return err_value
@@ -322,10 +339,12 @@ def build_portfolio_csp( min_investment, max_investment, risk_factor, min_expect
 
     constraints.append(Constraint(scope=variables, condition=lambda *values: sum(values) <= max_investment))
     constraints.append(Constraint(scope=variables, condition=lambda *values: sum(values) >= min_investment))
-    constraints.append(Constraint(scope=variables,condition=lambda *values: calculate_portfolio_volatility(*values) <= risk_factor))
-    #constraints.append(Constraint(scope=variables, condition=lambda *values: calculate_min_return(*values) >= min_expected_return))
-    #constraints.append(Constraint(scope=variables, condition=lambda *values: all(value >= 0 and value <= max_for_each for value in values)))
+    constraints.append(
+        Constraint(scope=variables, condition=lambda *values: calculate_portfolio_volatility(*values) <= risk_factor))
+    # constraints.append(Constraint(scope=variables, condition=lambda *values: calculate_min_return(*values) >= min_expected_return))
+    # constraints.append(Constraint(scope=variables, condition=lambda *values: all(value >= 0 and value <= max_for_each for value in values)))
     return CSP("Portfolio Optimization", variables, constraints)
+
 
 def csp_solver(csp):
     arc_solver = Arc_Consistency(csp)
@@ -336,6 +355,7 @@ def csp_solver(csp):
         print("Solution:", solution)
     """
     return all_solutions
+
 
 def load_model(dataset):
     model_path = f'models/model_{dataset}.pkl'
@@ -349,7 +369,7 @@ def load_model(dataset):
         return None
 
 def main():
-    csp = build_portfolio_csp(0, 60, 0.9)
+    csp = build_portfolio_csp(0, 60, 0.9, 0)
     solutions = csp_solver(csp)
     for solution in solutions:
         print("Solution: ", solution)
